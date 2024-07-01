6902d0d278075b7f3f159ad51ba0f404fd726c1e
Tomasz Banaś <tomasz.banas123@gmail.com>
2024-07-01 23:14:54 +0200

    add template scripts

---
+++ ReQuest/Assets/Scripts/TemplateScript.cs
@@ -0,0 +1,30 @@
+﻿public class TemplateScript
+{
+    // Public Constants
+
+    // Static Variables and Methods
+
+    // Public Variables
+
+    // Serialized Private Variables
+
+    // Injected Dependencies (using Zenject)
+
+    // Private Variables
+
+    // Properties
+
+    // Events
+
+    // Unity Callbacks
+
+    // Public Methods
+
+    // Virtual Methods
+
+    // Abstract Methods
+
+    // Private Methods
+
+    // Event Handlers
+}
