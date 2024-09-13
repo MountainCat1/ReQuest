ec2efa8e2e343e40039ddd6bd5a44abff222f472
Tomasz Banaś <tomasz.banas123@gmail.com>
2024-09-13 13:10:30 +0200

    limit the effect of CrtEffect

--- ReQuest/Assets/Scripts/CrtEffectController.cs
+++ ReQuest/Assets/Scripts/CrtEffectController.cs
@@ -70,15 +70,15 @@ public class CrtEffectController : MonoBehaviour
         }
         if(phase == 2)
         {
-            SetLayerWeight(0.005f);
+            SetLayerWeight(0f);
         }
         if(phase == 3)
         {
-            SetLayerWeight(0.01f);
+            SetLayerWeight(0.0f);
         }
         if(phase == 4)
         {
-            SetLayerWeight(0.02f);
+            SetLayerWeight(0.01f);
         }
         if(phase == 5)
         {
